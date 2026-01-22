29982471ee1ec340841429a71e2c9c21d4926996
Web Developer <jorge.zuluaga@udea.edu.co>
2026-01-22 14:28:48 -0500

    Relocate autoreload magics and bump version to 0.3.4

--- src/multineas/version.py
+++ src/multineas/version.py
@@ -7,4 +7,4 @@
 # License: GNU Affero General Public License v3 (AGPL-3.0)        #
 ##################################################################
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
